9145f453fce6aff0f2ca151d0b8a6e35e3c4cc55
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2023-07-15 03:41:35 -0400

    setup the the reading of paragraphs on big think

--- brain/app/webScraping/scraper.py
+++ brain/app/webScraping/scraper.py
@@ -1,9 +1,15 @@
-from bs4 import BeautifulSoup
-import requests
-
-def scrape(url):
-    print("Scraping URL: " + url)
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    print(soup.prettify())
-
+from bs4 import BeautifulSoup 
+import requests 
+from langchain.llms import OpenAI 
+passage = ""
+def scrape(url): 
+    llm = OpenAI(temperature=0.9) 
+    print("Scraping URL: " + url) 
+    response = requests.get(url) 
+    soup = BeautifulSoup(response.content, 'html.parser') 
+    bigThink(soup) # call the function tree for bigThink.com 
+def bigThink(soup): 
+    paragraphs = soup.find_all('p', class_="") 
+    for paragraph in paragraphs: 
+        print(paragraph.text) 
+        passage += paragraph.text #next step is to add this to the prompt passed to gpt api as a langchain parameter
