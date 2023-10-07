7fbe37f28f987e1165ca00d48fda5a57afab6252
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2023-10-07 11:58:53 -0400

    basic reactflow setup

--- client/src/pages/graph.tsx
+++ client/src/pages/graph.tsx
@@ -1,43 +1,17 @@
 import React, { useEffect, useState } from "react";
 import NoteBox from "@/components/NoteNode/noteBox";
 import Draggable from "react-draggable";
-import styles from "@/styles/Graph.module.scss";
+import styles from "../styles/Graph.module.scss";
 import PORT_NUMBER from "../constants/constants";
+import Flow from "@/components/Flow";
+import 'reactflow/dist/style.css';
 
 export default function Graph() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    console.log("fetching data");
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          `http://127.0.0.1:${PORT_NUMBER}/get-graph`,
-          {
-            method: "GET",
-          }
-        );
-
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const responseJson = await response.json(); // No need to JSON.parse(data)
-        setData(responseJson["data"]); // Set data directly
-        console.log(data); // Log the parsed data
-      } catch (error) {
-        console.error("Fetch error:", error);
-      }
-    };
-    fetchData();
-  }, []);
-
   return (
-    <>
-      <Draggable>
-        <div className={styles.box}>
-            <NoteBox />
-        </div>
-      </Draggable>
-    </>
+    //add height of 100% to div
+    <div className={styles.App}>
+      <Flow />
+      <h1>Graph</h1>
+    </div>
   );
 }
